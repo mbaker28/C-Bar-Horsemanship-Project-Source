36a1445ad19b851aed244cf1af9d1dc8696f4207
michael-johnson <michael@michaelandrewjohnson.net>
2016-02-05 16:48:54 -0600

    Setup the initial index view and urls.

--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index_public(request):
+    return HttpResponse("Welcome to the C-Bar Horsemanship TRC information database!")
