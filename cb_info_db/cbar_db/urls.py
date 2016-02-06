0ead522064f45136438ba41ec37542565b674449
michael-johnson <michael@michaelandrewjohnson.net>
2016-02-05 21:34:18 -0600

    Added comment to cbar_db/urls.py

--- cb_info_db/cbar_db/urls.py
+++ cb_info_db/cbar_db/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index_public, name='index-public'),
+    url(r'^', views.index_public, name='index-public'), # main directory
 ]
