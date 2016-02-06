3509781353f7b543722c467cddf9bdc7a78fc934
michael-johnson <michael@michaelandrewjohnson.net>
2016-02-05 20:58:37 -0600

    Added "/db.cbarhorsemanship" and "/db.cbarhorsemanship/public" to repo.

---
+++ passenger_wsgi.py
@@ -0,0 +1,16 @@
+import sys, os
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/cb_info_db')
+
+INTERP = os.path.expanduser("~/db.cbarhorsemanship.org/env/bin/python")
+
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
+sys.path.insert(0,'$HOME/db.cbarhorsemanship.org/env/bin')
+sys.path.insert(0,'$HOME/db.cbarhorsemanship.org/env/lib/python3.4/site-packages/django')
+sys.path.insert(0,'$HOME/db.cbarhorsemanship.org/env/lib/python3.4/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "cb_info_db.settings"
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
