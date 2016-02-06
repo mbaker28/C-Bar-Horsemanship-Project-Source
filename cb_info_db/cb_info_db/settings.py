1e2f63720c6cd7b694951b348d177bd0207fa0e6
michael-johnson <michael@michaelandrewjohnson.net>
2016-02-05 21:30:30 -0600

    Fixed missing comma in Django settings files

--- cb_info_db/cb_info_db/settings.py
+++ cb_info_db/cb_info_db/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'cbar_db.apps.CbarDbConfig'
+    'cbar_db.apps.CbarDbConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
