61ed65f27a0a8c24eb3a03edeb90056cf8697687
michael-johnson <michael@michaelandrewjohnson.net>
2016-02-05 21:55:38 -0600

    Changed python version to 3.5 in passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,8 +8,8 @@ INTERP = os.path.expanduser("~/db.cbarhorsemanship.org/env/bin/python")
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 sys.path.insert(0,'$HOME/db.cbarhorsemanship.org/env/bin')
-sys.path.insert(0,'$HOME/db.cbarhorsemanship.org/env/lib/python3.4/site-packages/django')
-sys.path.insert(0,'$HOME/db.cbarhorsemanship.org/env/lib/python3.4/site-packages')
+sys.path.insert(0,'$HOME/db.cbarhorsemanship.org/env/lib/python3.5/site-packages/django')
+sys.path.insert(0,'$HOME/db.cbarhorsemanship.org/env/lib/python3.5/site-packages')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "cb_info_db.settings"
 from django.core.wsgi import get_wsgi_application
