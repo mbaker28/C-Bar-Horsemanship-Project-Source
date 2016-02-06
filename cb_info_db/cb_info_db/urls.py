643ed8e38c4afd3741616c17d3cf0b869af3398b
michael-johnson <michael@michaelandrewjohnson.net>
2016-02-05 22:21:51 -0600

    Added missing '$' in cb_info_db/urls.py

--- cb_info_db/cb_info_db/urls.py
+++ cb_info_db/cb_info_db/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('cbar_db.urls')), # Ex: db.cbarhorsemanship.org/
+    url(r'^$', include('cbar_db.urls')), # Ex: db.cbarhorsemanship.org/
     url(r'^admin/', admin.site.urls), # Ex: db.cbarhorsemanship.org/admin/
 ]
