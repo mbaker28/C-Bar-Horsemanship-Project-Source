d803286bfbe4ef05ac3ddad7e4ec93a8af81e2e8
michael-johnson <michael@michaelandrewjohnson.net>
2016-03-17 18:42:23 -0500

    [Re #4] Added models for Participant table.

--- cb_info_db/cbar_db/models.py
+++ cb_info_db/cbar_db/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Participant(models.Model):
+    """ Main table for all Participants """
+
+    # Constants and Choices
+    NAME_LENGTH=75
+    MALE='M'
+    FEMALE='F'
+    GENDER_CHOICES=(
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    )
+    MINOR='M'
+    ADULT_WITH_GUARDIAN='G'
+    ADULT_WITHOUT_GUARDIAN='A'
+    MINOR_STATUS_CHOICES=(
+        (MINOR, 'Minor'),
+        (ADULT_WITH_GUARDIAN, 'Adult with guardian'),
+        (ADULT_WITHOUT_GUARDIAN, 'Independent adult')
+    )
+    PHONE_LENGTH=15
+
+    # Attributes
+    participant_ID=models.AutoField(primary_key=True) # Auto generated PK
+    name=models.CharField(max_length=NAME_LENGTH)
+    birth_date=models.DateField()
+    email=models.EmailField() # Auto-validation for email addresses
+    weight=models.DecimalField(max_digits=4, decimal_places=1) # Ex: 999.9
+    gender=models.CharField(max_length=1, choices=GENDER_CHOICES) # Ex: 'M', 'F'
+    guardian_name=models.CharField(max_length=NAME_LENGTH)
+    height=models.DecimalField(max_digits=4, decimal_places=1) # Ex: 999.9
+    minor_status=models.CharField(max_length=1, choices=MINOR_STATUS_CHOICES)
+    address_street=models.CharField(max_length=150)
+    address_city=models.CharField(max_length=50)
+    address_zip=models.CharField(max_length=6)
+    phone_home=models.CharField(max_length=PHONE_LENGTH)
+    phone_cell_work=models.CharField(max_length=PHONE_LENGTH)
+    school_institution=models.CharField(max_length=150, blank=True)
