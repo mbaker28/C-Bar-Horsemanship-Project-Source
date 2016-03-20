47c88cd376b92afc9e234f524697731f3bbc1821
michael-johnson <michael@michaelandrewjohnson.net>
2016-03-20 18:12:46 -0500

    [Re #6] Added views, URLs, and empty templates for the public forms

--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -2,4 +2,45 @@
 from django.shortcuts import render
 
 def index_public(request):
+    """ Website index view. """
     return render(request, 'cbar_db/index.html')
+
+
+def index_public_forms(request):
+    """ Public forms index view. """
+    return render(request, 'cbar_db/forms/public/public.html')
+
+
+def public_form_application(request):
+    """ Application form view. """
+    return render(request, 'cbar_db/forms/public/application.html')
+
+
+def public_form_med_release(request):
+    """ Medical Release form view. """
+    return render(request, 'cbar_db/forms/public/medical_release.html')
+
+
+def public_form_emerg_auth(request):
+    """ Emegency Medical Treatment Authorization form view. """
+    return render(request, 'cbar_db/forms/public/emergency_authorization.html')
+
+
+def public_form_liability(request):
+    """ Liability Release form view. """
+    return render(request, 'cbar_db/forms/public/liability.html')
+
+
+def public_form_media(request):
+    """ Media Release form view. """
+    return render(request, 'cbar_db/forms/public/media.html')
+
+
+def public_form_background(request):
+    """ Background Check Authorization form view. """
+    return render(request, 'cbar_db/forms/public/background.html')
+
+
+def public_form_seizure(request):
+    """ Seizure Evaluation form view. """
+    return render(request, 'cbar_db/forms/public/seizure.html')
