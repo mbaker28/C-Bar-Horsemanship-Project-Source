257d2a73b800b329b06492f0988c2496056ca870
michael-johnson <michael@michaelandrewjohnson.net>
2016-03-22 16:45:40 -0500

    [Re #10] Implemented initial Media Release backend.
    TODO:
    -Better post form (error and success) handling.
    -Make and run tests.

--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -1,6 +1,12 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+import logging
 from cbar_db import forms
+from cbar_db import models
+
+logger = logging.getLogger()
 
 def index_public(request):
     """ Website index view. """
@@ -37,17 +43,37 @@ def public_form_media(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = forms.MediaReleaseForm(request.POST)
+        form=forms.MediaReleaseForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            # Create an instance of the MediaRelease model to hold form data
+            logger.error("Creating MediaRelease instance...")
+            try:
+                form_data_media=models.MediaRelease(
+                    participant_id=models.Participant.objects.get(
+                        name=form.cleaned_data['name']
+                    ),
+                    consent=form.cleaned_data['consent'],
+                    signature=form.cleaned_data['signature'],
+                    date=form.cleaned_data['date']
+                )
+                logger.error("Saving MediaRelease instance...")
+                new_data=form_data_media.save()
+                logger.error("Sucessfully saved Media Release form")
+            except ObjectDoesNotExist:
+                """ Triggered if the PK found by name doesn't exist
+                    AKA: The participant record doesn't exist.
+                 """
+                logger.error(
+                    "Couldn't find Participant FK to save media release form."
+                )
+
+        # redirect to a new URL:
+        return HttpResponseRedirect('/')
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = forms.MediaReleaseForm()
+        form=forms.MediaReleaseForm()
 
     return render(request, 'cbar_db/forms/public/media.html', {'form': form})
 
