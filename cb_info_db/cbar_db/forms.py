f9d863c4c100a9ee70644ac5009c3477f67e8f78
michael-johnson <michael@michaelandrewjohnson.net>
2016-03-22 11:36:09 -0500

    [Re #10] Implemented Media Release form frontend

--- cb_info_db/cbar_db/forms.py
+++ cb_info_db/cbar_db/forms.py
@@ -2,7 +2,16 @@ from django import forms
 from cbar_db import models
 
 class MediaReleaseForm(forms.Form):
-    name = forms.CharField(
-        label='Your name',
-        max_length=20 # models.Participant.name.max_length
+    name=forms.CharField(
+        max_length=models.Participant._meta.get_field("name").max_length
     )
+
+    consent=forms.ChoiceField(
+        choices=models.MediaRelease._meta.get_field("consent").choices
+    )
+
+    signature=forms.CharField(
+        max_length=models.MediaRelease._meta.get_field("signature").max_length
+    )
+
+    date=forms.DateField()
