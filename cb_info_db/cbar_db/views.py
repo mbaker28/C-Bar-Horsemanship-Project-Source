6192e23dc2f3942aa88653e15674a0a9c5d825a6
ConnorCrout <connor.crout@okbu.edu>
2016-03-22 22:19:33 -0500

    [re #8] Implementing the Medical Authorization form W.I.P.

--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -23,7 +23,39 @@ def public_form_med_release(request):
 
 def public_form_emerg_auth(request):
     """ Emegency Medical Treatment Authorization form view. """
-    return render(request, 'cbar_db/forms/public/emergency_authorization.html')
+    # if this is a POST request we need to process the form data
+    if request.method -- 'POST':
+        # create a form instance and populate it with data from the request:
+        form=form.EmergencyMedicalReleaseForm(request.POST)
+        #chech whether it's valid:
+        if form.is_valid():
+            #create an instance of the EmergencyMedicalReleaseForm model to hold form data
+            logger.error("Creating Emergency Medical Release Form instance...")
+            try:
+                form_data_emerg_auth=models.EmergencyMedicalReleaseForm(
+                    participant_id=models.Participant.objects.get(
+                        name=form.cleaned_data['name']
+                    ),
+                    # TODO: birth_date needs to be updated in relavenent participant instance
+                )
+                logger.error("Saving Emergency Medical Release Form instance...")
+                new_data=form_data_media.save()
+                logger.error("Successfully saved Emergency Medical Release Form")
+            except objectDoesNotExsist:
+                """Triggered if the PK found by name doesn't exist
+                    AKA:The Participant record Doesn't exist.
+                """
+                logger.error(
+                    "Couldn't find Participant FK to save Emergency Medical Release Form"
+                    )
+                    #redirec to a new URL:
+                    return HttpResponseRedirect('/')
+
+                #if a GET (or any other method) we'll create a black for,
+            else:
+                form=forms.EmergencyMedicalReleaseForm()
+
+        return render(request, 'cbar_db/forms/public/media.html', {'form': form})
 
 
 def public_form_liability(request):
