63a55ae09b289a2d00b4cb3370f77027357b11da
michael-johnson <michael@michaelandrewjohnson.net>
2016-03-23 20:30:10 -0500

    [Re #8] Cleaned up 'EmergencyMedicalReleaseForm' in forms.py

--- cb_info_db/cbar_db/forms.py
+++ cb_info_db/cbar_db/forms.py
@@ -10,49 +10,45 @@ class EmergencyMedicalReleaseForm(forms.Form):
 
     # Shite stored in MedicalInfo
     primary_physician_name=forms.CharField(
-        max_length=models.MedicalInfo._meta.field("primary_physician_name").max_length
+        max_length=models.MedicalInfo._meta.get_field("primary_physician_name").max_length
     )
     primary_physician_phone=forms.CharField(
-        max_length=models.MedicalInfo._meta.field("primary_physician_phone").max_length
+        max_length=models.MedicalInfo._meta.get_field("primary_physician_phone").max_length
     )
 
     # Shite stored in AuthorizeEmergencyMedicalTreatment
     pref_medical_facility=forms.CharField(
         max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
-            .field("pref_medical_facility").max_length
+            .get_field("pref_medical_facility").max_length
         )
     )
     insurance_provider=forms.CharField(
         max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
-            .field("insurance_provider").max_length
+            .get_field("insurance_provider").max_length
         )
     )
     insurance_policy_num=forms.CharField(
         max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
-            .field("insurance_policy_num").max_length
+            .get_field("insurance_policy_num").max_length
         )
     )
     emerg_contact_name=forms.CharField(
         max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
-            .field("emerg_contact_name").max_length
+            .get_field("emerg_contact_name").max_length
         )
     )
     emerg_contact_phone=forms.CharField(
         max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
-            .field("emerg_contact_phone").max_length
+            .get_field("emerg_contact_phone").max_length
         )
     )
     emerg_contact_relation=forms.CharField(
         max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
-            .field("emerg_contact_relation").max_length
+            .get_field("emerg_contact_relation").max_length
         )
     )
-    consents_emerg_med_treatment=forms.CharField(
-        choices=(models.AuthorizeEmergencyMedicalTreatment._meta
-            .field("consents_emerg_med_treatment").choices
-        )
-    )
+    consents_emerg_med_treatment=forms.CharField()
     date=forms.DateField()
     signature=forms.CharField(
-        max_length=models.MedicalInfo._meta.field("signature").max_length
+        max_length=models.MedicalInfo._meta.get_field("signature").max_length
     )
