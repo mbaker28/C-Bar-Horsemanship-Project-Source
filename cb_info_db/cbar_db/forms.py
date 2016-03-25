96521dfa3880481d8fbb7e519ade144c0613e0d5
michael-johnson <michael@michaelandrewjohnson.net>
2016-03-25 16:50:21 -0500

    [Re #8] WIP: Backend for Emergency Authorization Form

--- cb_info_db/cbar_db/forms.py
+++ cb_info_db/cbar_db/forms.py
@@ -2,21 +2,25 @@ from django import forms
 from cbar_db import models
 
 class EmergencyMedicalReleaseForm(forms.Form):
-    # Shit stored in Participant
+    # Stored in Participant
     name=forms.CharField(
         max_length=models.Participant._meta.get_field("name").max_length
     )
-    birth_date=forms.DateField()
+    birth_date=forms.DateField(initial="mm/dd/yy")
 
-    # Shite stored in MedicalInfo
+    # Stored in MedicalInfo
     primary_physician_name=forms.CharField(
-        max_length=models.MedicalInfo._meta.get_field("primary_physician_name").max_length
+        max_length=(models.MedicalInfo._meta
+            .get_field("primary_physician_name").max_length
+        )
     )
     primary_physician_phone=forms.CharField(
-        max_length=models.MedicalInfo._meta.get_field("primary_physician_phone").max_length
+        max_length=(models.MedicalInfo._meta
+            .get_field("primary_physician_phone").max_length
+        )
     )
 
-    # Shite stored in AuthorizeEmergencyMedicalTreatment
+    # Stored in AuthorizeEmergencyMedicalTreatment
     pref_medical_facility=forms.CharField(
         max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
             .get_field("pref_medical_facility").max_length
@@ -47,8 +51,14 @@ class EmergencyMedicalReleaseForm(forms.Form):
             .get_field("emerg_contact_relation").max_length
         )
     )
-    consents_emerg_med_treatment=forms.CharField()
+    consents_emerg_med_treatment=forms.ChoiceField(
+        choices=(models.AuthorizeEmergencyMedicalTreatment._meta
+            .get_field("consents_emerg_med_treatment").choices
+        )
+    )
     date=forms.DateField()
     signature=forms.CharField(
-        max_length=models.MedicalInfo._meta.get_field("signature").max_length
+        max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
+            .get_field("signature").max_length
+        )
     )
--- cb_info_db/cbar_db/models.py
+++ cb_info_db/cbar_db/models.py
@@ -86,6 +86,12 @@ LIKERT_LIKE_CHOICES_NO_PC=(
     (ATTEMPTS, "Attempts")
 )
 
+CONSENT="Y"
+NO_CONSENT="N"
+CONSENT_CHOICES=(
+    (CONSENT, "consent"),
+    (NO_CONSENT, "do not consent")
+)
 
 class Participant(models.Model):
     participant_id=models.AutoField(primary_key=True) # Auto generated PK
@@ -243,13 +249,6 @@ class Diagnosis(models.Model):
 
 
 class MediaRelease(models.Model):
-    CONSENT="Y"
-    NO_CONSENT="N"
-    CONSENT_CHOICES=(
-        (CONSENT, "consent"),
-        (NO_CONSENT, "do not consent")
-    )
-
     class Meta: # Sets up PK as (participant_id, date)
         unique_together=(("participant_id","date"))
 
@@ -293,8 +292,9 @@ class AuthorizeEmergencyMedicalTreatment(models.Model):
     alt_emerg_procedure=models.CharField(max_length=500, null=True)
     consents_emerg_med_treatment=models.CharField(
         max_length=1,
-        choices=YES_NO_CHOICES
+        choices=CONSENT_CHOICES
     )
+    signature=models.CharField(max_length=NAME_LENGTH)
 
 
 class EvalHorsemanship(models.Model):
