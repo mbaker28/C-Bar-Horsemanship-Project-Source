82620f5f2c5e7f17eff61555d216ad151a868b05
michael-johnson <michael@michaelandrewjohnson.net>
2016-03-28 16:38:02 -0500

    [Re #10] Added birth_date field to 'MediaReleaseForm' and 'media.html'.
    (Name and birth date are used to ID a participant when not using the auto generated PK. For example, when a user without an account is entering form data)

--- cb_info_db/cbar_db/forms.py
+++ cb_info_db/cbar_db/forms.py
@@ -5,13 +5,11 @@ class MediaReleaseForm(forms.Form):
     name=forms.CharField(
         max_length=models.Participant._meta.get_field("name").max_length
     )
-
+    birth_date=forms.DateField()
     consent=forms.ChoiceField(
         choices=models.MediaRelease._meta.get_field("consent").choices
     )
-
     signature=forms.CharField(
         max_length=models.MediaRelease._meta.get_field("signature").max_length
     )
-
     date=forms.DateField()
