75297fbda9cb89deed4557bbe7545db283ec9cea
michael-johnson <michael@michaelandrewjohnson.net>
2016-03-28 18:13:40 -0500

    [Re #7] WIP Medical Info back-end: Implemented 'GET' portion of view

--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -1,5 +1,6 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from cbar_db import forms
 
 def index_public(request):
     """ Website index view. """
@@ -17,9 +18,28 @@ def public_form_application(request):
 
 
 def public_form_med_release(request):
-    """ Medical Release form view. """
-    return render(request, 'cbar_db/forms/public/medical_release.html')
+    """ Medical Release form view. Handles viewing and saving the form.
 
+    Viewing form (GET): Display the form
+    Saving form (POST):
+        -Do something
+    """
+
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # Do stuff
+        print("Doing something...")
+    else:
+        # If request type is GET (or any other method) create a blank form and
+        # display it:
+        form=forms.MedicalReleaseForm()
+        return render(
+            request,
+            'cbar_db/forms/public/medical_release.html',
+            {
+                'form': form
+            }
+        )
 
 def public_form_emerg_auth(request):
     """ Emegency Medical Treatment Authorization form view. """
