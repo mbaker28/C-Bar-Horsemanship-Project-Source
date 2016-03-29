6594b32e8d6c2cfa70e08e7d736ec98b42b70c3c
mkbaker <mattbakerk@gmail.com>
2016-03-29 10:23:40 -0500

    [Re #9] Implemented liability form in views.py

--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -17,7 +17,8 @@ def public_form_application(request):
 
 
 def public_form_med_release(request):
-    """ Medical Release form view. """
+    """ Medical Release form view.
+    """
     return render(request, 'cbar_db/forms/public/medical_release.html')
 
 
@@ -27,9 +28,73 @@ def public_form_emerg_auth(request):
 
 
 def public_form_liability(request):
-    """ Liability Release form view. """
-    return render(request, 'cbar_db/forms/public/liability.html')
+    """ Liability Release form view.
 
+     If Participant exists:
+        Create new instance of MediaReleaseForm and save it to the DB
+    Else:
+        Give an error
+    """
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form=forms.LiabilityReleaseForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # Create an instance of the LiabilityRelease model to hold form data
+            try:
+                # Find the participant that matches the name and birth date from
+                # the form data:
+                participant=models.Participant.objects.get(
+                    name=form.cleaned_data['name'],
+                    birth_date=form.cleaned_data['birth_date']
+                )
+
+            except ObjectDoesNotExist:
+                # The participant doesn't exist.
+                # Set the error message and redisplay the form:
+                return render(
+                    request,
+                    "cbar_db/forms/public/liability.html",
+                    {
+                        'form': form,
+                        'error_text': ("The requested participant isn't in the"
+                        " database."),
+                    }
+                )
+
+            # Create a new LiabilityRelease for the participant and save it:
+            form_data_liability=models.LiabilityRelease(
+                participant_id=participant,
+                signature=form.cleaned_data['signature'],
+                date=form.cleaned_data['date']
+            )
+            form_data_liability.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect('/')
+
+        else:
+            # The form is not valid.
+            # Set the error message and redisplay the form:
+            return render(
+                request,
+                "cbar_db/forms/public/liability.html",
+                {
+                    'form': form,
+                    'error_text': "Error validating form.",
+                }
+            )
+
+    else:
+        # If request type is GET (or any other method) create a blank form.
+        form=forms.MediaReleaseForm()
+
+        return render(
+            request,
+            'cbar_db/forms/public/liability.html',
+            {'form': form}
+        )
 
 def public_form_media(request):
     """ Media Release form view. """
