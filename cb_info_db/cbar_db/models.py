49fc23b3867490bc901666b950210bc345cb3d64
michael-johnson <michael@michaelandrewjohnson.net>
2016-03-29 17:26:40 -0500

    REVERT: Merged branch 'master' into 'Issue-10-Implement-Media-Release-form'
    (reverted from commit 0c9f84270e0fe3e090dcf9c5ac8fef694f3f1d2e)

--- cb_info_db/cbar_db/models.py
+++ cb_info_db/cbar_db/models.py
@@ -86,12 +86,6 @@ LIKERT_LIKE_CHOICES_NO_PC=(
     (ATTEMPTS, "Attempts")
 )
 
-CONSENT="Y"
-NO_CONSENT="N"
-CONSENT_CHOICES=(
-    (CONSENT, "consent"),
-    (NO_CONSENT, "do not consent")
-)
 
 class Participant(models.Model):
     participant_id=models.AutoField(primary_key=True) # Auto generated PK
@@ -299,9 +293,8 @@ class AuthorizeEmergencyMedicalTreatment(models.Model):
     alt_emerg_procedure=models.CharField(max_length=500, null=True)
     consents_emerg_med_treatment=models.CharField(
         max_length=1,
-        choices=CONSENT_CHOICES
+        choices=YES_NO_CHOICES
     )
-    signature=models.CharField(max_length=NAME_LENGTH)
 
 
 class EvalHorsemanship(models.Model):
--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -1,3 +1,4 @@
+# from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.exceptions import ObjectDoesNotExist
@@ -5,18 +6,7 @@ import logging
 from cbar_db import forms
 from cbar_db import models
 
-ERROR_TEXT_PARTICIPANT_NOT_FOUND=(
-    "The requested participant isn't in the database."
-)
-ERROR_TEXT_MEDICAL_INFO_NOT_FOUND=(
-    "The requested participant does not have their medical information on file."
-    " Please fill out a medical release first."
-)
-ERROR_TEXT_FORM_INVALID=(
-    "Error validating form."
-)
-
-loggeyMcLogging=logging.getLogger(__name__)
+logger = logging.getLogger()
 
 def index_public(request):
     """ Website index view. """
@@ -39,126 +29,8 @@ def public_form_med_release(request):
 
 
 def public_form_emerg_auth(request):
-    """ Emegency Medical Treatment Authorization form view. Handles viewing and
-     saving the form.
-
-    Viewing form (GET): Display the form
-    Saving form (POST):
-        If participant exists:
-            Update record in Participant
-            Update record in MedicalInfo
-            Create new record in AuthorizeEmergencyMedicalTreatment
-        Else:
-            Give an error
-    """
-
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        loggeyMcLogging.error("Request is of type POST")
-        # Create a form instance and populate it with data from the request:
-        form=forms.EmergencyMedicalReleaseForm(request.POST)
-
-        # Check whether the form data entered is valid:
-        if form.is_valid():
-            loggeyMcLogging.error("The form is valid")
-            # Find the participant's record based on their (name, birth_date):
-            try:
-                participant=models.Participant.objects.get(
-                    name=form.cleaned_data['name'],
-                    birth_date=form.cleaned_data['birth_date']
-                )
-            except ObjectDoesNotExist:
-                # The participant doesn't exist.
-                # Set the error message and redisplay the form:
-                return render(
-                    request,
-                    "cbar_db/forms/public/emergency_authorization.html",
-                    {
-                        'form': form,
-                        'error_text': (ERROR_TEXT_PARTICIPANT_NOT_FOUND),
-                    }
-                )
-
-            # If no exception, the participant exists. Update their records:
-            try:
-                # If the participant has a MedicalInfo record, retrieve it:
-                medical_info=models.MedicalInfo.objects.get(
-                    participant_id=participant
-                )
-
-                # Update the fields:
-                medical_info.primary_physician_name=(
-                    form.cleaned_data['primary_physician_name']
-                )
-                medical_info.primary_physician_phone=(
-                    form.cleaned_data['primary_physician_phone']
-                )
-
-                # Save the updated record
-                medical_info.save()
-            except ObjectDoesNotExist:
-                # The participant has no MedicalInfo record, prompt them to fill
-                # out the Medical Release first.
-                return render(
-                    request,
-                    "cbar_db/forms/public/emergency_authorization.html",
-                    {
-                        'form': form,
-                        'error_text': (
-                            ERROR_TEXT_MEDICAL_INFO_NOT_FOUND
-                        ),
-                    }
-                )
-
-            # Create a new AuthorizeEmergencyMedicalTreatment instance for the
-            # participant and save it:
-            authorize_emerg_medical=models.AuthorizeEmergencyMedicalTreatment(
-                participant_id=participant,
-                pref_medical_facility=(
-                    form.cleaned_data['pref_medical_facility']
-                ),
-                insurance_provider=form.cleaned_data['insurance_provider'],
-                insurance_policy_num=form.cleaned_data['insurance_policy_num'],
-                emerg_contact_name=form.cleaned_data['emerg_contact_name'],
-                emerg_contact_phone=form.cleaned_data['emerg_contact_phone'],
-                emerg_contact_relation=(
-                    form.cleaned_data['emerg_contact_relation']
-                ),
-                consents_emerg_med_treatment=(
-                    form.cleaned_data['consents_emerg_med_treatment']
-                ),
-                date=form.cleaned_data['date'],
-                signature=form.cleaned_data['signature']
-            )
-            authorize_emerg_medical.save()
-
-            # Redirect to the home page:
-            return HttpResponseRedirect('/')
-
-        else:
-            # The form is not valid
-            loggeyMcLogging.error("The form is NOT valid")
-
-            return render(
-                request,
-                "cbar_db/forms/public/emergency_authorization.html",
-                {
-                    'form': form,
-                    'error_text': ERROR_TEXT_FORM_INVALID,
-                }
-            )
-
-    else:
-        # If request type is GET (or any other method) create a blank form.
-        form=forms.EmergencyMedicalReleaseForm()
-
-        return render(
-            request,
-            'cbar_db/forms/public/emergency_authorization.html',
-            {
-                'form': form,
-            }
-        )
+    """ Emegency Medical Treatment Authorization form view. """
+    return render(request, 'cbar_db/forms/public/emergency_authorization.html')
 
 
 def public_form_liability(request):
