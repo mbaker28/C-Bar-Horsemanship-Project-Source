ae395a5705654d822774b0345c66c7d7a99424e1
mkbaker <mattbakerk@gmail.com>
2016-03-28 20:55:57 -0500

    [Re #9] Cleaned up html file and backend.

--- cb_info_db/cbar_db/forms.py
+++ cb_info_db/cbar_db/forms.py
@@ -3,12 +3,9 @@ from cbar_db import models
 
 class LiabilityReleaseForm(forms.Form):
     name = forms.CharField(
-        label = 'Your name',
-        max_length=models.Participant.name.max_length
+        max_length=models.Participant._meta.get_field("name").max_length
     )
-
     signature=forms.CharField(
         max_length=models.MedicalInfo._meta.get_field("signature").max_length
     )
-
     date = forms.DateField()
