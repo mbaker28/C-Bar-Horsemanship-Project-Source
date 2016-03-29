69fe02a5b4d1407a0ba77bde0aba5249c76bde78
ConnorCrout <connor.crout@okbu.edu>
2016-03-29 11:35:59 -0500

    Merged branch 'master' into Issue11-Background-Check-Authorization-form

--- cb_info_db/cbar_db/forms.py
+++ cb_info_db/cbar_db/forms.py
@@ -17,3 +17,66 @@ class BackgroundCheckForm(forms.Form):
             .get_field("driver_license_num").max_length
         )
     )
+
+
+class EmergencyMedicalReleaseForm(forms.Form):
+    # Stored in Participant
+    name=forms.CharField(
+        max_length=models.Participant._meta.get_field("name").max_length
+    )
+    birth_date=forms.DateField()
+
+    # Stored in MedicalInfo
+    primary_physician_name=forms.CharField(
+        max_length=(models.MedicalInfo._meta
+            .get_field("primary_physician_name").max_length
+        )
+    )
+    primary_physician_phone=forms.CharField(
+        max_length=(models.MedicalInfo._meta
+            .get_field("primary_physician_phone").max_length
+        )
+    )
+
+    # Stored in AuthorizeEmergencyMedicalTreatment
+    pref_medical_facility=forms.CharField(
+        max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
+            .get_field("pref_medical_facility").max_length
+        )
+    )
+    insurance_provider=forms.CharField(
+        max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
+            .get_field("insurance_provider").max_length
+        )
+    )
+    insurance_policy_num=forms.CharField(
+        max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
+            .get_field("insurance_policy_num").max_length
+        )
+    )
+    emerg_contact_name=forms.CharField(
+        max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
+            .get_field("emerg_contact_name").max_length
+        )
+    )
+    emerg_contact_phone=forms.CharField(
+        max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
+            .get_field("emerg_contact_phone").max_length
+        )
+    )
+    emerg_contact_relation=forms.CharField(
+        max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
+            .get_field("emerg_contact_relation").max_length
+        )
+    )
+    consents_emerg_med_treatment=forms.ChoiceField(
+        choices=(models.AuthorizeEmergencyMedicalTreatment._meta
+            .get_field("consents_emerg_med_treatment").choices
+        )
+    )
+    date=forms.DateField()
+    signature=forms.CharField(
+        max_length=(models.AuthorizeEmergencyMedicalTreatment._meta
+            .get_field("signature").max_length
+        )
+    )
