b19eaa3046766efe1656767d872597daf77667ea
michael-johnson <michael@michaelandrewjohnson.net>
2016-04-02 10:18:53 -0500

    Merge branch 'master' into Issue-9-Implement-Liability-Release-Form
    Conflicts:
    	cb_info_db/cbar_db/forms.py
    	cb_info_db/cbar_db/views.py
    	cb_info_db/tests.py

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # C-Bar Information Database - SOURCE CODE
 
-[![Run Status](https://api.shippable.com/projects/56b97bb71895ca447473a0cc/badge?branch=master)](https://app.shippable.com/projects/56b97bb71895ca447473a0cc)
+[![Build Status](https://api.shippable.com/projects/56b97bb71895ca447473a0cc/badge?branch=master)](https://app.shippable.com/projects/56b97bb71895ca447473a0cc)
+
+[![Coverage Status](https://api.shippable.com/projects/56b97bb71895ca447473a0cc/coverageBadge?branch=master)](https://app.shippable.com/projects/56b97bb71895ca447473a0cc)
 
 This repository contains the source code for the C-Bar Information Database
  Project.
@@ -12,67 +14,93 @@ This repository contains the source code for the C-Bar Information Database
 env\scripts\activate
 ```
 
-2) Run the development server. Navigate to the `cb_info_db` folder and run:
+2) To run the development server and test on your local machine, navigate to the `cb_info_db` folder and run:
 ```bash
 python manage.py runserver
 ```
 This will allow you to test on the machine the server is running on by visiting
- `localhost:8000` in the browser. If you want to access the development server
- on a different device on the local network (i.e to test on a tablet or phone),
- run this instead:
+ `localhost:8000` in the browser.
+
+To run the development server and test on a different device, run this instead:
 ```bash
 python manage.py runserver 0.0.0.0:8000
 ```
 Then visit `{your_computers_ip_number}:8000`
  (for example `172.16.104.114:8000`) in a web browser on the other device. You
- can find your IP address by running `ipconfig`. Keep in mind this will be
- accessible to every device on the local network.
+ can find your IP address by running `ipconfig`. __Keep in mind this will be
+ accessible to every device on the local network.__
 
 3) Do your develop. Hail Python!
-
-If you need to make changes using manage.py, run them in the command prompt like
-so:
-```bash
-python manage.py
-```
-If you are having problems, make sure you are in the root folder of the
-repository and `(env)` is at the start of your command prompt. If not, redo step
-1 and then try again.
-
-## Testing user
-
-This account is needed to access any portion of the
-[db.cbarhorsemanship.org](http://db.cbarhorsemanship.org) while in development.
-
-* Username: test
-* Password: inw@EngMZUz!EG3hQC^P5!Z4t$tQm9gD
 
 ## Coding Style
 [This document](CODING_STYLE.md) outlines guidelines to follow regarding coding
  styles used. It includes how we will do variable naming, use whitespace, etc.
 
-## Setting up Gulp.js
+## Tests
+[Shippable](https://app.shippable.com/): Continuous Integration provider.
+ Essentially it runs the tests every time a commit is pushed to the repository.
 
-1) Pull the repository
+### Running tests
 
-2) Install gulp globally. Run:
-
-```bash
-npm install --global gulp-cli
-```
-
-3) Install gulp locally. Open a terminal in the root level of the `source`
- repository and Run:
+After activating `(env)`, from a command prompt in the `cb_info_db` folder that contains `manage.py`, run:
 
 ```bash
-npm install --save-dev gulp
+python manage.py test
 ```
 
-4) Install the required gulp extensions. Run:
-
+You can also run just one specific test class by running:
 ```bash
-npm install --save-dev gulp-less gulp-watch gulp-uglify gulp-cssnano del
+python manage.py test tests.ClassNameToTest
 ```
+
+You can even run one specific test function by running this:
+```bash
+python manage.py test tests.ClassNameToTest.function_to_test
+```
+
+### Coverage Reports
+
+Coverage reports are useful for telling you what needs to be tested. To generate
+ a coverage report, navigate to the `cb_info_db` folder and run this:
+```bash
+coverage run ./manage.py test
+coverage html
+```
+Then open the `cb_info_db/htmlcov/index.html` in your web browser.
+
+Red highlighted lines denote code that hasn't been tested in one of the
+ automated tests.
+
+## Git
+This is the workflow for making a contribution to this Git repository.
+
+1. Checkout the `master` branch.
+2. Pull changes from `master` (if any).
+3. Create a new branch from `master`. Name it
+`issue-###-name-of-the-issue`. For example:
+`issue-3-fix-form-overlap-in-internet-explorer`.
+4. Checkout this branch.
+5. Make some changes.
+6. Commit the changes to this branch. Start each commit message with
+`[Re #{issue number}]`. For example, `[Re #1]`.
+7. Repeat steps 5&6 until the issue should be resolved.
+8. Pull changes on the `master` branch (if any).
+9. Merge the `master` branch onto the `issue-###-...` branch.
+10. Make sure that everything is still functioning as expected.
+11. Push the `issue-###-...` branch to the repository.
+12. Create a pull request with the same name as this branch. Add at least one
+team member as a reviewer (typically Michael).
+13. The pull request should then be reviewed by at least one other team member
+and approved.
+14. Once the pull request is approved, Michael will merge the pull request
+onto master.
+
+### Branches
+
+ * `master` is the main working development branch. Any pushes to the server
+ will be made from this branch.
+ * `Issue-{number}-...` branches are branches containing new or modified code
+ that address the relevant issue {number}.
 
 ## Setting up Development Environment
 Instructions [here](SETUP.md).
@@ -80,43 +108,15 @@ Instructions [here](SETUP.md).
 ## Things you should probably have installed
 
 * [SmartGit](http://www.syntevo.com/smartgit/): Git client.
-* [Atom](https://atom.io/): Text editor. Install it and then run `apm install color-picker autocomplete-python hard-wrap multi-cursor pdf-view pigments python-tools terminal-plus file-icons` to get Michael's preferred set of extensions installed.
+* [Atom](https://atom.io/): Text editor. Install it and then run `apm install color-picker autocomplete-python hard-wrap multi-cursor pdf-view pigments python-tools terminal-plus file-icons django-templates` to get Michael's preferred set of extensions installed.
 * [Slack](http://cbar-capstone.slack.com)
 
-## Tests
-* [Shippable](https://app.shippable.com/): Continuous Integration provider.
- Essentially it runs the tests every time a commit is pushed to the repository.
+## Testing user
 
-## Git
-This is the workflow for making a contribution to this Git repository.
+This account is needed to access any portion of the [db.cbarhorsemanship.org](http://db.cbarhorsemanship.org) site while in development.
 
-1. Checkout the ```master``` branch.
-2. Pull changes from ```master``` (if any).
-3. Create a new branch from ```master```. Name it
-```issue-###-name-of-the-issue```. For example:
-```issue-3-fix-form-overlap-in-internet-explorer```.
-4. Checkout this branch.
-5. Make some changes.
-6. Commit the changes to this branch. Start each commit message with
- ```[Re #{issue number}]```. For example, ```[Re #1]```.
-7. Repeat steps 5&6 until the issue should be resolved.
-8. Pull changes on the ```master``` branch (if any).
-9. Merge the ```master``` branch onto the ```issue-###-...``` branch.
-10. Make sure that everything is still functioning as expected.
-11. Push the ```issue-###-...``` branch to the repository.
-12. Create a pull request with the same name as this branch. Add at least one
- team member as a reviewer (typically Michael).
-13. The pull request should then be reviewed by at least one other team member
- and approved.
-14. Once the pull request is approved, Michael will merge the pull request
- onto master.
-
-### Branches
-
-* `master` is the main working development branch. Any pushes to the server
- will be made from this branch.
-* `Issue-{number}-...` branches are branches containing new or modified code
- that address the relevant issue {number}.
+* Username: test
+* Password: inw@EngMZUz!EG3hQC^P5!Z4t$tQm9gD
 
 ### Issues
 
--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -1,10 +1,22 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.exceptions import ObjectDoesNotExist
 import logging
 from cbar_db import forms
 from cbar_db import models
+
+ERROR_TEXT_PARTICIPANT_NOT_FOUND=(
+    "The requested participant isn't in the database."
+)
+ERROR_TEXT_MEDICAL_INFO_NOT_FOUND=(
+    "The requested participant does not have their medical information on file."
+    " Please fill out a medical release first."
+)
+ERROR_TEXT_FORM_INVALID=(
+    "Error validating form."
+)
+
+loggeyMcLogging=logging.getLogger(__name__)
 
 def index_public(request):
     """ Website index view. """
@@ -22,14 +34,266 @@ def public_form_application(request):
 
 
 def public_form_med_release(request):
-    """ Medical Release form view.
-    """
-    return render(request, 'cbar_db/forms/public/medical_release.html')
+    """ Medical Release form view. Handles viewing and saving the form.
 
+    Viewing form (GET): Display the form
+    Saving form (POST):
+        -Do something
+    """
+
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        loggeyMcLogging.error("Request is of type POST")
+        # Create a form instance and populate it with data from the request:
+        form=forms.MedicalReleaseForm(request.POST)
+
+        # Check whether the form data entered is valid:
+        if form.is_valid():
+            loggeyMcLogging.error("The form is valid")
+            # Find the participant's record based on their (name, birth_date):
+            try:
+                participant=models.Participant.objects.get(
+                    name=form.cleaned_data['signature'],
+                    birth_date=form.cleaned_data['birth_date']
+                )
+            except ObjectDoesNotExist:
+                # The participant doesn't exist.
+                # Set the error message and redisplay the form:
+                return render(
+                    request,
+                    "cbar_db/forms/public/medical_release.html",
+                    {
+                        'form': form,
+                        'error_text': (ERROR_TEXT_PARTICIPANT_NOT_FOUND),
+                    }
+                )
+
+            medical_info=models.MedicalInfo(
+                participant_id=participant,
+                date=form.cleaned_data["date"],
+                primary_physician_name=(
+                    form.cleaned_data["primary_physician_name"]
+                ),
+                primary_physician_phone=(
+                    form.cleaned_data["primary_physician_phone"]
+                ),
+                last_seen_by_physician_date=(
+                    form.cleaned_data["last_seen_by_physician_date"]
+                ),
+                last_seen_by_physician_reason=(
+                    form.cleaned_data["last_seen_by_physician_reason"]
+                ),
+                allergies_conditions_that_exclude=(
+                    form.cleaned_data["allergies_conditions_that_exclude"]
+                ),
+                allergies_conditions_that_exclude_description=(
+                    form.cleaned_data["allergies_conditions_that_exclude"
+                        "_description"]
+                ),
+                heat_exhaustion_stroke=(
+                    form.cleaned_data["heat_exhaustion_stroke"]
+                ),
+                tetanus_shot_last_ten_years=(
+                    form.cleaned_data["tetanus_shot_last_ten_years"]
+                ),
+                seizures_last_six_monthes=(
+                    form.cleaned_data["seizures_last_six_monthes"]
+                ),
+                currently_taking_any_medication=(
+                    form.cleaned_data["currently_taking_any_medication"]
+                ),
+                doctor_concered_re_horse_activites=(
+                    form.cleaned_data["doctor_concered_re_horse_activites"]
+                ),
+                physical_or_mental_issues_affecting_riding=(
+                    form.cleaned_data["physical_or_mental_issues_affecting"
+                        "_riding"]
+                ),
+                physical_or_mental_issues_affecting_riding_description=(
+                    form.cleaned_data["physical_or_mental_issues_affecting"
+                        "_riding_description"]
+                ),
+                restriction_for_horse_activity_last_five_years=(
+                    form.cleaned_data["restriction_for_horse_activity_last"
+                        "_five_years"]
+                ),
+                restriction_for_horse_activity_last_five_years_description=(
+                    form.cleaned_data["restriction_for_horse_activity_last_five"
+                        "_years_description"]
+                ),
+                present_restrictions_for_horse_activity=(
+                    form.cleaned_data["present_restrictions_for_horse_activity"]
+                ),
+                limiting_surgeries_last_six_monthes=(
+                    form.cleaned_data["limiting_surgeries_last_six_monthes"]
+                ),
+                signature=(form.cleaned_data["signature"])
+            )
+            medical_info.save()
+
+            medication_one=models.Medication(
+                medical_info_id=medical_info,
+                medication_name=form.cleaned_data["medication_one_name"],
+                duration_taken=form.cleaned_data["medication_one_duration"],
+                frequency=form.cleaned_data["medication_one_frequency"]
+            )
+            medication_one.save()
+            medication_two=models.Medication(
+                medical_info_id=medical_info,
+                medication_name=form.cleaned_data["medication_two_name"],
+                duration_taken=form.cleaned_data["medication_two_duration"],
+                frequency=form.cleaned_data["medication_two_frequency"]
+            )
+            medication_two.save()
+
+            # Redirect to the home page:
+            return HttpResponseRedirect('/')
+
+        else:
+            # The form is not valid
+            loggeyMcLogging.error("The form is NOT valid")
+
+            return render(
+                request,
+                "cbar_db/forms/public/medical_release.html",
+                {
+                    'form': form,
+                    'error_text': ERROR_TEXT_FORM_INVALID,
+                }
+            )
+    else:
+        # If request type is GET (or any other method) create a blank form and
+        # display it:
+        form=forms.MedicalReleaseForm()
+        return render(
+            request,
+            'cbar_db/forms/public/medical_release.html',
+            {
+                'form': form
+            }
+        )
 
 def public_form_emerg_auth(request):
-    """ Emegency Medical Treatment Authorization form view. """
-    return render(request, 'cbar_db/forms/public/emergency_authorization.html')
+    """ Emegency Medical Treatment Authorization form view. Handles viewing and
+     saving the form.
+
+    Viewing form (GET): Display the form
+    Saving form (POST):
+        If participant exists:
+            Update record in Participant
+            Update record in MedicalInfo
+            Create new record in AuthorizeEmergencyMedicalTreatment
+        Else:
+            Give an error
+    """
+
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        loggeyMcLogging.error("Request is of type POST")
+        # Create a form instance and populate it with data from the request:
+        form=forms.EmergencyMedicalReleaseForm(request.POST)
+
+        # Check whether the form data entered is valid:
+        if form.is_valid():
+            loggeyMcLogging.error("The form is valid")
+            # Find the participant's record based on their (name, birth_date):
+            try:
+                participant=models.Participant.objects.get(
+                    name=form.cleaned_data['name'],
+                    birth_date=form.cleaned_data['birth_date']
+                )
+            except ObjectDoesNotExist:
+                # The participant doesn't exist.
+                # Set the error message and redisplay the form:
+                return render(
+                    request,
+                    "cbar_db/forms/public/emergency_authorization.html",
+                    {
+                        'form': form,
+                        'error_text': (ERROR_TEXT_PARTICIPANT_NOT_FOUND),
+                    }
+                )
+
+            # If no exception, the participant exists. Update their records:
+            try:
+                # If the participant has a MedicalInfo record, retrieve it:
+                medical_info=models.MedicalInfo.objects.get(
+                    participant_id=participant
+                )
+
+                # Update the fields:
+                medical_info.primary_physician_name=(
+                    form.cleaned_data['primary_physician_name']
+                )
+                medical_info.primary_physician_phone=(
+                    form.cleaned_data['primary_physician_phone']
+                )
+
+                # Save the updated record
+                medical_info.save()
+            except ObjectDoesNotExist:
+                # The participant has no MedicalInfo record, prompt them to fill
+                # out the Medical Release first.
+                return render(
+                    request,
+                    "cbar_db/forms/public/emergency_authorization.html",
+                    {
+                        'form': form,
+                        'error_text': (
+                            ERROR_TEXT_MEDICAL_INFO_NOT_FOUND
+                        ),
+                    }
+                )
+
+            # Create a new AuthorizeEmergencyMedicalTreatment instance for the
+            # participant and save it:
+            authorize_emerg_medical=models.AuthorizeEmergencyMedicalTreatment(
+                participant_id=participant,
+                pref_medical_facility=(
+                    form.cleaned_data['pref_medical_facility']
+                ),
+                insurance_provider=form.cleaned_data['insurance_provider'],
+                insurance_policy_num=form.cleaned_data['insurance_policy_num'],
+                emerg_contact_name=form.cleaned_data['emerg_contact_name'],
+                emerg_contact_phone=form.cleaned_data['emerg_contact_phone'],
+                emerg_contact_relation=(
+                    form.cleaned_data['emerg_contact_relation']
+                ),
+                consents_emerg_med_treatment=(
+                    form.cleaned_data['consents_emerg_med_treatment']
+                ),
+                date=form.cleaned_data['date'],
+                signature=form.cleaned_data['signature']
+            )
+            authorize_emerg_medical.save()
+
+            # Redirect to the home page:
+            return HttpResponseRedirect('/')
+
+        else:
+            # The form is not valid
+            loggeyMcLogging.error("The form is NOT valid")
+
+            return render(
+                request,
+                "cbar_db/forms/public/emergency_authorization.html",
+                {
+                    'form': form,
+                    'error_text': ERROR_TEXT_FORM_INVALID,
+                }
+            )
+
+    else:
+        # If request type is GET (or any other method) create a blank form.
+        form=forms.EmergencyMedicalReleaseForm()
+
+        return render(
+            request,
+            'cbar_db/forms/public/emergency_authorization.html',
+            {
+                'form': form,
+            }
+        )
 
 
 def public_form_liability(request):
@@ -102,14 +366,130 @@ def public_form_liability(request):
         )
 
 def public_form_media(request):
-    """ Media Release form view. """
-    return render(request, 'cbar_db/forms/public/media.html')
+    """ Media Release form view.
+
+    If Participant exists:
+        Create new instance of MediaReleaseForm and save it to the DB
+    Else:
+        Give an error
+    """
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form=forms.MediaReleaseForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # Create an instance of the MediaRelease model to hold form data
+            try:
+                # Find the participant that matches the name and birth date from
+                # the form data:
+                participant=models.Participant.objects.get(
+                    name=form.cleaned_data['name'],
+                    birth_date=form.cleaned_data['birth_date']
+                )
+
+            except ObjectDoesNotExist:
+                # The participant doesn't exist.
+                # Set the error message and redisplay the form:
+                return render(
+                    request,
+                    "cbar_db/forms/public/media.html",
+                    {
+                        'form': form,
+                        'error_text': ("The requested participant isn't in the"
+                        " database."),
+                    }
+                )
+
+            # Create a new MediaRelease for the participant and save it:
+            form_data_media=models.MediaRelease(
+                participant_id=participant,
+                consent=form.cleaned_data['consent'],
+                signature=form.cleaned_data['signature'],
+                date=form.cleaned_data['date']
+            )
+            form_data_media.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect('/')
+
+        else:
+            # The form is not valid.
+            # Set the error message and redisplay the form:
+            return render(
+                request,
+                "cbar_db/forms/public/media.html",
+                {
+                    'form': form,
+                    'error_text': "Error validating form.",
+                }
+            )
+
+    else:
+        # If request type is GET (or any other method) create a blank form.
+        form=forms.MediaReleaseForm()
+
+        return render(
+            request,
+            'cbar_db/forms/public/media.html',
+            {'form': form}
+        )
 
 
 def public_form_background(request):
     """ Background Check Authorization form view. """
-    return render(request, 'cbar_db/forms/public/background.html')
+    if request.method == 'POST':
+        loggeyMcLogging.error("Request is of type POST")
+        form=forms.BackgroundCheckForm(request.POST)
 
+        if form.is_valid():
+            loggeyMcLogging.error("The form is valid")
+            try:
+                participant=models.Participant.objects.get(
+                    name=form.cleaned_data['name'],
+                    birth_date=form.cleaned_data['birth_date']
+                )
+            except ObjectDoesNotExist:
+                return render(
+                    request,
+                    "cbar_db/forms/public/background.html",
+                    {
+                        'form': form,
+                        'error_text': (ERROR_TEXT_PARTICIPANT_NOT_FOUND),
+                    }
+                )
+
+            public_form_background=models.BackgroundCheck(
+                participant_id=participant,
+                date=form.cleaned_data['date'],
+                signature=form.cleaned_data['signature'],
+                driver_license_num=form.cleaned_data['driver_license_num']
+            )
+            public_form_background.save()
+
+            # Redirect to the home page:
+            return HttpResponseRedirect('/')
+
+        else:
+            form=forms.BackgroundCheckForm()
+            return render(
+                request,
+                'cbar_db/forms/public/background.html',
+                {
+                    'form': form,
+                    'error_text':ERROR_TEXT_FORM_INVALID,
+                }
+            )
+    else:
+        form=forms.BackgroundCheckForm()
+
+        return render (
+            request,
+            'cbar_db/forms/public/background.html',
+            {
+                'form':form,
+            }
+        )
 
 def public_form_seizure(request):
     """ Seizure Evaluation form view. """
