46b18629fd433d7a9cdc4a59047dcc5ce3acc678
michael-johnson <michael@michaelandrewjohnson.net>
2016-04-02 17:58:24 -0500

    [Re #21] Set session expiry parameters in settings files

--- cb_info_db/cb_info_db/settings.py
+++ cb_info_db/cb_info_db/settings.py
@@ -110,6 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 LOGIN_URL='user-login'
+SESSION_COOKIE_AGE=30 * 60 #30 minutes
+SESSION_SAVE_EVERY_REQUEST=True #Reset the expiration timer with every request
+SESSION_EXPIRE_AT_BROWSER_CLOSE=True #Logout the user if they close their browser
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
