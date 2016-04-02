d992cc75b27b2ba40b1108b9aff0ffd3c85c853f
michael-johnson <michael@michaelandrewjohnson.net>
2016-04-02 10:00:40 -0500

    [Re #9] WIP: Adding LiabilityRelease model

--- cb_info_db/cbar_db/forms.py
+++ cb_info_db/cbar_db/forms.py
@@ -5,10 +5,10 @@ class LiabilityReleaseForm(forms.Form):
     name = forms.CharField(
         max_length=models.Participant._meta.get_field("name").max_length
     )
-    
+
     birth_date = forms.DateField()
 
     signature=forms.CharField(
-        max_length=models.MedicalInfo._meta.get_field("signature").max_length
+        max_length=models.LiabilityRelease._meta.get_field("signature").max_length
     )
     date = forms.DateField()
