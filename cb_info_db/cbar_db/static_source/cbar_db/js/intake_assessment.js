8f557b293ead600bde0fe743ae27612aaa335082
michael-johnson <michael@michaelandrewjohnson.net>
2016-04-27 22:33:43 -0500

    [Re #48] Initial implementation of jQuery to show/hide sections of the Rider Intake Assessment.

--- cb_info_db/cbar_db/static_source/cbar_db/js/intake_assessment.js
+++ cb_info_db/cbar_db/static_source/cbar_db/js/intake_assessment.js
@@ -1,9 +1,22 @@
+// This function is called after all HTML DOM elements have loaded:
 $( document ).ready(function() {
-    $("#general_information").hide();
     $("#premounted").hide();
     $("#sidewalkers_required").hide();
-});
 
-$("#section").change(function() {
-    alert("Section selection changed.");
+    $("#section").change(function() {
+        alert("Section selection changed.");
+
+        if ($("#section").val() == "general_information") {
+            alert("Section selection is 'general_information'.");
+            $("#general_information").show().siblings("div").hide();
+        };
+        if ($("#section").val() == "premounted") {
+            alert("Section selection is 'premounted'.");
+            $("#premounted").show().siblings("div").hide();
+        };
+        if ($("#section").val() == "sidewalkers_required") {
+            alert("Section selection is 'sidewalkers_required'.");
+            $("#sidewalkers_required").show().siblings("div").hide();
+        };
+    });
 });
