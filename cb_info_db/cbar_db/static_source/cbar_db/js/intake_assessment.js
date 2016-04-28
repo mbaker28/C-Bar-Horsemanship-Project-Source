564d764468df90a6b704f63d8a8d52deb0be210c
michael-johnson <michael@michaelandrewjohnson.net>
2016-04-27 21:29:21 -0500

    [Re #48] WIP: Implementing jQuery to show/hide sections of the Rider Intake Assessment.

---
+++ cb_info_db/cbar_db/static_source/cbar_db/js/intake_assessment.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $("#general_information").hide();
+    $("#premounted").hide();
+    $("#sidewalkers_required").hide();
+});
+
+$("#section").change(function() {
+    alert("Section selection changed.");
+});
