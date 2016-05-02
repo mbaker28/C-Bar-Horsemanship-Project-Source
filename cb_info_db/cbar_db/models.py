ca21095dab4245bca7d808bd7717d3d88e46d1ec
michael-johnson <michael@michaelandrewjohnson.net>
2016-05-02 13:30:11 -0500

    [Re #37] Added 'null=True' parameter to 'action_to_take_allow_how_long' field in SeizureEval model.

--- cb_info_db/cbar_db/models.py
+++ cb_info_db/cbar_db/models.py
@@ -1094,7 +1094,8 @@ class SeizureEval(models.Model):
     action_to_take_allow_time=models.NullBooleanField()
     action_to_take_allow_time_how_long=models.DecimalField(
         max_digits=2,
-        decimal_places=0
+        decimal_places=0,
+        null=True
     )
     action_to_take_report_immediately=models.NullBooleanField()
     action_to_take_send_note=models.NullBooleanField()
