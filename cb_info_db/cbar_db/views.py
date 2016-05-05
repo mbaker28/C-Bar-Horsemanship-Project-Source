5b612049323eaf562e8ea46214ea4f07faa7fd01
michael-johnson <michael@michaelandrewjohnson.net>
2016-05-05 15:17:59 -0500

    [Re #62] Updated 'views.private_form_session_plan' to save the 'SessionPlanInd' model

--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -2172,8 +2172,11 @@ def private_form_session_plan(request, participant_id):
             session_plan.save()
 
             session_ind=models.SessionPlanInd(
+                participant_id=participant,
+                date=form.cleaned_data["date"],
                 horse_leader=form.cleaned_data['horse_leader']
             )
+            session_ind.save()
 
             session_goals=models.SessionGoals(
                 participant_id=participant,
