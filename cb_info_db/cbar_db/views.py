be1b2ac62c31f81d5ccab7c628af37c76ff2526f
michael-johnson <michael@michaelandrewjohnson.net>
2016-05-06 08:52:00 -0500

    [Re #65] Modified medical release view to find the participant record based on the name field instead of the signature field. Modified tests as required.

--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -165,7 +165,7 @@ def public_form_med_release(request):
             # Find the participant's record based on their (name, birth_date):
             try:
                 participant=models.Participant.objects.get(
-                    name=form.cleaned_data['signature'],
+                    name=form.cleaned_data['name'],
                     birth_date=form.cleaned_data['birth_date']
                 )
             except ObjectDoesNotExist:
