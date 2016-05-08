dc587fed51a7dd179a471b55afe9ed7017be4bfc
michael-johnson <michael@michaelandrewjohnson.net>
2016-05-07 22:19:21 -0500

    [Re #72][Re #85] Commented out debugging calls to 'alert()' in 'intake_assessment.js'.

--- cb_info_db/cbar_db/static_source/cbar_db/js/intake_assessment.js
+++ cb_info_db/cbar_db/static_source/cbar_db/js/intake_assessment.js
@@ -11,16 +11,16 @@ $( document ).ready(function() {
             selected in the dropdown menu.
         */
         if ($("#section").val() == "general_information") {
-            alert("Section selection is 'general_information'."); // DEBUGGING
+            // alert("Section selection is 'general_information'."); // DEBUGGING
             $("#general_information").show().siblings("div").hide();
         };
         if ($("#section").val() == "premounted") {
-            alert("Section selection is 'premounted'."); // DEBUGGING
+            // alert("Section selection is 'premounted'."); // DEBUGGING
             $("#premounted").show().siblings("div").hide();
             $("#premounted").children().show(); // Show all the child elements
         };
         if ($("#section").val() == "sidewalkers_required") {
-            alert("Section selection is 'sidewalkers_required'."); // DEBUGGING
+            // alert("Section selection is 'sidewalkers_required'."); // DEBUGGING
             $("#sidewalkers_required").show().siblings("div").hide();
         };
 
@@ -29,17 +29,17 @@ $( document ).ready(function() {
             is selected in the dropdown menu.
         */
         if ($("#section").val() == "premounted_posture") {
-            alert("Section selection is 'premounted_posture'."); // DEBUGGING
+            // alert("Section selection is 'premounted_posture'."); // DEBUGGING
             $("#premounted").show().siblings("div").hide();
             $("#posture").show().siblings("div").hide();
         };
         if ($("#section").val() == "premounted_ambulatory") {
-            alert("Section selection is 'premounted_ambulatory'."); // DEBUGGING
+            // alert("Section selection is 'premounted_ambulatory'."); // DEBUGGING
             $("#premounted").show().siblings("div").hide();
             $("#ambulatory").show().siblings("div").hide();
         };
         if ($("#section").val() == "premounted_gait") {
-            alert("Section selection is 'premounted_gait'."); // DEBUGGING
+            // alert("Section selection is 'premounted_gait'."); // DEBUGGING
             $("#premounted").show().siblings("div").hide();
             $("#gait").show().siblings("div").hide();
         };
