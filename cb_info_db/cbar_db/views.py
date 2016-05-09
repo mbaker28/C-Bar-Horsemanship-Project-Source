b2d2339640031163339dc9976f9b67b44a063d2e
mkbaker <mattbakerk@gmail.com>
2016-05-09 16:36:38 -0500

    [Re #81] Implemented views for Rider Evaluation Checklist report. Added code to participant record view to pass the private forms to it.

--- cb_info_db/cbar_db/views.py
+++ cb_info_db/cbar_db/views.py
@@ -46,6 +46,9 @@ ERROR_TEXT_OBS_EVAL_NOT_AVALIABLE=(
 ERROR_TEXT_SES_PLAN_NOT_AVALIABLE=(
     "The Session Plan requested is not available."
 )
+ERROR_TEXT_RIDER_EVAL_CHECKLIST_NOT_AVAILABLE=(
+    "The Rider Evaluation Checklist requested is not available."
+)
 ERROR_TEXT_DB_INTEGRITY=(
     "An internal database error has occured and the form could not be saved."
     " Please verify that you have not already filled out a form for this"
@@ -1740,6 +1743,24 @@ def participant_record(request, participant_id):
         )
     )
 
+    # Find our Participant's ObservationEvaluation instances
+    observation_evaluations=(models.EvalAttitude.objects.filter(
+        participant_id=participant
+        )
+    )
+
+    # Find our Participant's SessionPlanInd instances
+    session_plans=(models.SessionPlanInd.objects.filter(
+        participant_id=participant
+        )
+    )
+
+    # Find our Participant's EvalRidingExercises instances
+    rider_eval_checklists=(models.EvalRidingExercises.filter(
+        participant_id=participant
+        )
+    )
+
     return render(
         request,
         "cbar_db/admin/reports/participant.html",
@@ -1750,7 +1771,10 @@ def participant_record(request, participant_id):
             "emergency_authorizations": emergency_authorizations,
             "liability_releases": liability_releases,
             "background_checks": background_checks,
-            "seizure_evals": seizure_evals
+            "seizure_evals": seizure_evals,
+            "observation_evaluations": observation_evaluations,
+            "session_plans": session_plans,
+            "rider_eval_checklists": rider_eval_checklists
         }
     )
 
@@ -2649,6 +2673,69 @@ def report_session_plan(request, participant_id, year, month, day):
         "cbar_db/admin/reports/report_session_plan.html",
         {
             "session_plan": session_plan,
+            "participant": participant
+        }
+    )
+
+@login_required
+def report_rider_eval_checklist(request, participant_id, year, month, day):
+    """ Displays a the data entered in a previous Rider Evaluation Checklist
+        form. """
+
+    # Find the participant's Participant record:
+    try:
+        participant=models.Participant.objects.get(
+            participant_id=participant_id
+        )
+    except ObjectDoesNotExist:
+        # The participant doesn't exist.
+        # Set the error message and redisplay the form:
+        return render(
+            request,
+            "cbar_db/admin/reports/report_rider_eval_checklist.html",
+            {
+                'error_text': (ERROR_TEXT_PARTICIPANT_NOT_FOUND),
+            }
+        )
+
+    # Parse the Observation Evaluation's date from the URL attributes
+    try:
+        loggeyMcLogging.error("year, month, day=" + year + "," + month + "," + day)
+        date=time.strptime(year + "/" + month + "/" + day, "%Y/%m/%d")
+        loggeyMcLogging.error("Date=" + str(date))
+    except:
+        loggeyMcLogging.error("Couldn't parse the date")
+        # The requested date can't be parsed
+        return render(
+            request,
+            "cbar_db/admin/reports/report_rider_eval_checklist.html",
+            {
+                'error_text': ERROR_TEXT_INVALID_DATE,
+            }
+        )
+
+    # Find the ObservationEvaluation record:
+    try:
+        rider_eval_checklist=models.EvalRidingExercises.objects.get(
+            participant_id=participant,
+            date=time.strftime("%Y-%m-%d", date)
+        )
+    except ObjectDoesNotExist:
+        # The MediaRelease doesn't exist.
+        # Set the error message and redisplay the form:
+        return render(
+            request,
+            "cbar_db/admin/reports/report_rider_eval_checklist.html",
+            {
+                'error_text': ERROR_TEXT_RIDER_EVAL_CHECKLIST_NOT_AVAILABLE,
+            }
+        )
+
+    return render(
+        request,
+        "cbar_db/admin/reports/report_rider_eval_checklist.html",
+        {
+            "rider_eval_checklist": rider_eval_checklist,
             "participant": participant
         }
     )
